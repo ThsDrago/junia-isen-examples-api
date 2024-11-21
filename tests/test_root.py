137a31df35a9c821937013620a38ee3fd9c09bb9
Hector de Tocqueville <hector.grouard-de-tocqueville@outscale.com>
2024-11-21 17:28:39 +0100

    update tests for /quotes route

--- tests/test_root.py
+++ tests/test_root.py
@@ -8,3 +8,17 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"Hello": "World"}
+
+def test_read_quotes():
+    # Tester la route "/quotes"
+    response = client.get("/quotes")
+    
+    # Afficher la réponse pour inspection
+    print(response.json())
+    
+    # Assurez-vous que la réponse a le code de statut 200
+    assert response.status_code == 200
+    
+    # Assurez-vous que la réponse contient une clé "quotes" et qu'elle n'est pas vide
+    assert "quotes" in response.json()
+    assert len(response.json()["quotes"]) > 0
