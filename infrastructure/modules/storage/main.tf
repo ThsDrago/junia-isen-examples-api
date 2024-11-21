311eea7eb6430bc126ef3149b0c9e3a3e57b111d
Hector de Tocqueville <hector.grouard-de-tocqueville@outscale.com>
2024-11-21 17:50:02 +0100

    fix : env var in workflow

--- infrastructure/modules/storage/main.tf
+++ infrastructure/modules/storage/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "blob_storage" {
-  name                     = "storageaccount${var.random_suffix}"
+  name                     = "ccpstorageaccount"
   resource_group_name      = var.resource_group_name
   location                 = var.location
   account_tier             = "Standard"
