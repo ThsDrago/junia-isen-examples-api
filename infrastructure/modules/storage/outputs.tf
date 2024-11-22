b47709f997d45dbefac91109dd062eb6ec2b951b
allan <chan.ross@student.junia.com>
2024-11-22 12:53:38 +0100

    test

--- infrastructure/modules/storage/outputs.tf
+++ infrastructure/modules/storage/outputs.tf
@@ -5,3 +5,8 @@ output "blob_storage_account_name" {
 output "blob_container_name" {
   value = azurerm_storage_container.blob_container.name
 }
+
+output "storage_account_url" {
+  description = "The URL of the Azure Storage Account"
+  value       = azurerm_storage_account.example.primary_blob_endpoint
+}
